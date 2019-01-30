2c21e5cd67d361bbbe5ad2e58d8920ab766192d2
fumito-s <fumito.shimokawa@gmail.com>
2019-01-30 16:01:02 +0900

    add Default Text to UIButton

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK:Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 
 
 }
